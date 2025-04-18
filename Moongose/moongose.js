44cfd3bf5127befdd6032e35bf8c104951b69026
neev-ahuja <neevahuja88@gmail.com>
2025-04-18 21:15:35 +0530

    Update moongose.js

--- Moongose/moongose.js
+++ Moongose/moongose.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/url', { useNewUrlParser: true, useUnifiedTopology: true }, () => console.log('Connected to MongoDB'));
+// mongoose.connect('mongodb://localhost:27017/url', { useNewUrlParser: true, useUnifiedTopology: true }, () => console.log('Connected to MongoDB'));
+
+
+mongoose.connect('mongodb+srv://neevahuja:y0BLFOsnZCErff86@cluster-1.ejpikke.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-1', { useNewUrlParser: true, useUnifiedTopology: true }, () => console.log('Connected to MongoDB'));
 
 
 const schema = new mongoose.Schema({
